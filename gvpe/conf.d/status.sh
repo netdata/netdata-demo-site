c0418954cd1bd837db95f293283f770140e534f6
Costa Tsaousis (ktsaou) <costa@tsaousis.gr>
2017-04-11 22:29:14 +0300

    complete gvpe status monitoring

--- gvpe/conf.d/status.sh
+++ gvpe/conf.d/status.sh
@@ -3,16 +3,41 @@
 export PATH="${PATH}:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin"
 
 set -e
-
-cd $(dirname "$0")/status
+ME="${0}"
+cd $(dirname "${ME}")/status
 
 now=$(date +%s)
 
-printf "%-30s %-15s %-15s %-6s %-20s\n" "Name" "VPN IP" "REAL IP" "STATUS" "DATE"
+EVENTS=0
+NODES_ALL=0
+NODES_UP=0
+NODES_DOWN=0
+LAST="NEVER"
 
-for x in $(ls * | sort -n)
+[ -f status ] && . ./status
+
+cat <<EOF
+
+GVPE Status on ${MYNODENAME} (Node No ${MYNODEID})
+
+Total Events: ${EVENTS}
+Last Event: $(date --date=@${timestamp} "+%Y-%m-%d %H:%M:%S")
+
+Up ${NODES_UP}, Down ${NODES_DOWN}, Total ${NODES_ALL} nodes
+
+EOF
+
+printf "%3s %-30s %-15s %-15s %-6s %-20s\n" \
+	"ID" "Name" "VPN IP" "REAL IP" "STATUS" "SINCE"
+
+for x in $(ls -t *)
 do
-	. $x
-	printf "%-30s %-15s %-15s %-6s %-20s\n" "$name" "$ip" "$rip" "$status" "$(date --date=@${timestamp} "+%Y-%m-%d %H:%M:%S")"
+	[ "${x}" = "status" ] && continue
+	[ "${x}" = "${MYNODEID}" ] && continue
+
+	. ./${x}
+	printf "%3u %-30s %-15s %-15s %-6s %-20s\n" \
+		"$((x))" "${name}" "${ip}" "${rip}" "${status}" \
+		"$(date --date=@${timestamp} "+%Y-%m-%d %H:%M:%S")"
 done
 
