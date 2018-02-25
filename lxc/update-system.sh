33ae892dc9120bb2e3ddd8b996e772c605063ca3
Costa Tsaousis (ktsaou) <costa@tsaousis.gr>
2018-02-26 00:23:56 +0200

    optimized system updates

--- lxc/update-system.sh
+++ lxc/update-system.sh
@@ -5,31 +5,32 @@ do
 	case "${x}" in
 		arch*)
 			echo "${x}: pacman -Syu"
-			lxc-attach -n "${x}" -- pacman --noconfirm -Syu
+			lxc-attach -n "${x}" -- /bin/sh -c 'yes | pacman -Syu'
 			;;
 
 		alpine*)
 			echo "${x}: apk update"
-			lxc-attach -n "${x}" -- apk update
-			lxc-attach -n "${x}" -- apk upgrade
+			lxc-attach -n "${x}" -- /bin/sh -c 'apk update && apk upgrade'
 			;;
 
 		centos*|fedora*)
 			echo "${x}: yum -y update"
-			lxc-attach -n "${x}" -- yum -y update
-			lxc-attach -n "${x}" -- yum -y upgrade
+			lxc-attach -n "${x}" -- /bin/sh -c 'yum -y update && yum -y upgrade'
 			;;
 
 		debian*|ubuntu*)
 			echo "${x}: apt-get update && apt-get -yq dist-upgrade"
-			lxc-attach -n "${x}" -- apt-get -y update
-			lxc-attach -n "${x}" -- apt-get -y dist-upgrade
+			lxc-attach -n "${x}" -- /bin/sh -c 'apt-get -y update && apt-get -y dist-upgrade'
 			;;
 
 		gentoo*)
 			echo "${x}: emerge -uDNv world"
-			lxc-attach -n "${x}" -- emerge --sync
-			lxc-attach -n "${x}" -- emerge -uDNv world
+			lxc-attach -n "${x}" -- /bin/sh -c 'emerge --sync && emerge -uDNv world'
+			;;
+
+		*suse*)
+			echo "${x}: zypper refresh && zypper update"
+			lxc-attach -n "${x}" -- /bin/sh -c 'zypper --non-interactive refresh && zypper --non-interactive update'
 			;;
 
 		*)
